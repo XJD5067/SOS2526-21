3b8fe3d0f87e27aff9f3ea0dd48a9232b69563ad
QXB8295 <martorcha@alum.us.es>
2026-03-23 16:10:09 +0100

    status del cholera eliminado

--- src/frontend/src/routes/cholera-stats/[country]/[year]/+page.svelte
+++ src/frontend/src/routes/cholera-stats/[country]/[year]/+page.svelte
@@ -72,17 +72,17 @@ onMount(async () =>  {getCholeraStat(); }); //que se carga al iniciar la pagina
 
 {#if resultStatus == 200 || resultStatus == 201}
 <div class="alert success">
-  <strong>Status {resultStatus}</strong> — Operación realizada con éxito
+  <strong>EXITO</strong> — Operación realizada con éxito
 </div>
 {/if}
 {#if resultStatus == 400}
 <div class="alert error">
-  <strong>Status {resultStatus}</strong> — Campos sin rellenar
+  <strong>ERROR</strong> — Campos sin rellenar
 </div>
 {/if}
 {#if resultStatus == 404}
 <div class="alert error">
-  <strong>Status {resultStatus}</strong> — No existe la estadística del país {country} y año {year}
+  <strong>ERROR</strong> — No existe la estadística del país {country} y año {year}
 </div>
 {/if}
 
