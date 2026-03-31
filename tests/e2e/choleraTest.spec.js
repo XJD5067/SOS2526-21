b61a6443ebab9dca85e0aae763156c3f5578124a
ugstudios-games <ugstudios.games@gmail.com>
2026-03-31 12:26:29 +0200

    test frontend ddlrf

--- tests/e2e/choleraTest.spec.js
+++ tests/e2e/choleraTest.spec.js
@@ -1,38 +1,8 @@
-import { test, expect } from '@playwright/test';
-let app = "http://localhost:3000";
-
-test('main page has right title', async ({page}) => {
-    await page.goto(app);
-
-    await expect(page).toHaveTitle(/Grupo 21 API/);
-})
-test('aids frontend show list', async ({ page }) => {
-    await page.goto(app);
-
-    await page.hover('.dropdown');
-
-    await page.getByRole('link', {name: 'AIDS'}).click();
-
-    await expect(page).toHaveTitle(/AIDs/);
-
-    await page.getByRole('button', {name: 'iniciales'}).click();
-
-    const dataRowsCount = await page.getByTestId('dataRow').count();
-
-    expect(dataRowsCount).toBeGreaterThan(0);
-  
-});
-
-
-
-
-
-
-
 //----------------------------------------------------------------------------------------------------------------------------
 //----------------------------FRONT CHOLERA TESTS-----------------------------------------------------------------------------------
 //----------------------------------------------------------------------------------------------------------------------------
-
+import { test, expect } from '@playwright/test';
+let app = "http://localhost:3000";
 
 test('cholera stats frontend shows cholera list', async ({ page }) => {
     await page.goto(app);
